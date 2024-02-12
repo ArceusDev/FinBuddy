e70d7fe40663b51cce9e6a993daf859c16ed4b23
Hamza Afzal <95589034+ArceusDev@users.noreply.github.com>
2024-02-13 03:47:40 +0500

    Initial commit

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Card.css'
+
+interface Props {
+    companyName: string;
+    ticker: string;
+    price: number;
+}
+
+const Card : React.FC<Props> = ({
+    companyName, 
+    ticker, 
+    price
+}: Props) : JSX.Element => {
+  return (
+    <div className='card'>
+        <img src="https://picsum.photos/200/300" alt="" />
+        <div className='details'>
+            <h2>{companyName} ({ticker})</h2>
+            <p>${price}</p>
+        </div>
+        <p className="info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, debitis.</p>
+    </div>
+  )
+}
+
+export default Card
